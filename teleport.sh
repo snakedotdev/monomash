3ebde9c9205f19c39099c10494f8d4b78462fcd5
Peter Fein <pete@wearpants.org>
2021-06-04 12:43:16 -0400

    some fixes

--- teleport.sh
+++ teleport.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-
 ## bring a $branch from old repo over to monorepo
 ##
-## teleport.sh <oldrepo path> <oldrepo branch> <monorepo path> <monorepo branch>
+## teleport.sh <oldrepo path> <oldrepo branch> <oldrepo main branch> <monorepo path> <monorepo branch> <path-in-monorepo>
 ##
 ## Example:
-## teleport.sh ~/pete/oldrepo myfeature ~/pete/monorepo main
+## teleport.sh ~/pete/oldrepo myfeature develop ~/pete/monorepo main ~/pete/monorepo/oldrepo
+#              1              2         3       4               5     6
 ##
-
 set -exu -o pipefail
-
 oldname=$(basename $1)
 branch=$2
-patchfile=~/teleport-$oldname-$branch.patch
-
+resolved_branch=$(echo $2 | sed s:/:_:g)
+patchfile=~/teleport-$oldname-${resolved_branch}.patch
+echo $patchfile
 pushd $1
 git switch $branch
-git diff $4..$branch > $patchfile
+git diff --binary $3..$branch > $patchfile
 popd
-
-pushd $3
-git checkout $4
+pushd $4
+git checkout $5
 git clean -di
 git checkout -b $branch
 pushd $oldname
-patch -p1 < $patchfile
+git apply -p1 $patchfile --directory $6
+#patch -p1 < $patchfile
 git add .
 popd
 git commit -am "Squashed branch $branch from old repo $oldname"
